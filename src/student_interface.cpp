90a4a8fbca20851c07b196b4aa05ccc75b318192
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-10-22 11:15:17 -0400

    Partial implementation till Extrinsic Calibration

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -3,24 +3,185 @@
 
 #include <stdexcept>
 #include <sstream>
+
+#include <vector>
+#include <atomic>
+#include <unistd.h>
+
+#include <experimental/filesystem>
+#include <sstream>
 namespace student {
+
+  static cv::Mat bg_img;
+  static std::vector<cv::Point2f> result;
+  static std::string name;
+  static std::atomic<bool> done;
+  static int n;
+  static double show_scale = 1.0;
+
+  void mouseCallback(int event, int x, int y, int, void* p)
+  {
+    if (event != cv::EVENT_LBUTTONDOWN || done.load()) return;
+    
+    result.emplace_back(x*show_scale, y*show_scale);
+    cv::circle(bg_img, cv::Point(x,y), 20/show_scale, cv::Scalar(0,0,255), -1);
+    cv::imshow(name.c_str(), bg_img);
+
+    if (result.size() >= n) {
+      usleep(500*1000);
+      done.store(true);
+    }
+  }
+
+  std::vector<cv::Point2f> pickNPoints(int n0, const cv::Mat& img)
+  {
+    result.clear();
+    std::cout << "in student interface pickNPoints" << std::endl;
+    cv::Size small_size(img.cols/show_scale, img.rows/show_scale);
+    cv::resize(img, bg_img, small_size);
+    //bg_img = img.clone();
+    name = "Pick " + std::to_string(n0) + " points";
+    cv::imshow(name.c_str(), bg_img);
+    cv::namedWindow(name.c_str());
+    n = n0;
+
+    done.store(false);
+
+    cv::setMouseCallback(name.c_str(), &mouseCallback, nullptr);
+    while (!done.load()) {
+      cv::waitKey(500);
+    }
+
+    cv::destroyWindow(name.c_str());
+    return result;
+  }
+
 
  void loadImage(cv::Mat& img_out, const std::string& config_folder){  
    throw std::logic_error( "STUDENT FUNCTION - LOAD IMAGE - NOT IMPLEMENTED" );
  }
 
  void genericImageListener(const cv::Mat& img_in, std::string topic, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - IMAGE LISTENER - NOT CORRECTLY IMPLEMENTED" );
+    static size_t id = 0;
+    static bool init = false;
+    static std::string folder_path;
+
+    if(!init){      
+      bool exist = true;
+      int i = 0;
+      while(exist && i < 1000){
+        std::stringstream ss;
+        ss << config_folder << "/camera_image" << std::setw(3) << std::setfill('0') << i << "/";
+        folder_path = ss.str();
+
+        exist = std::experimental::filesystem::exists(folder_path);
+
+        i++;        
+      }
+      
+      if(i > 999 || !std::experimental::filesystem::create_directories(folder_path)){
+        throw std::logic_error( "NO EMTY FOLDER" );
+      }
+
+      init = true;
+    }
+          
+    cv::imshow( topic, img_in);
+    char c;
+    c = cv::waitKey(30);
+    
+    std::stringstream img_file;
+    switch (c) {      
+    case 's':   
+      img_file << folder_path << std::setfill('0') 
+          << std::setw(3)  << (id++) << ".jpg";
+      cv::imwrite( img_file.str(), img_in );
+
+      std::cout << "Saved image " << img_file.str() << std::endl;
+      break;
+    default:
+        break;
+    }
+    //throw std::logic_error( "STUDENT FUNCTION - IMAGE LISTENER - NOT CORRECTLY IMPLEMENTED" );
   }
 
   bool extrinsicCalib(const cv::Mat& img_in, std::vector<cv::Point3f> object_points, const cv::Mat& camera_matrix, cv::Mat& rvec, cv::Mat& tvec, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - NOT IMPLEMENTED" );   
+    std::string file_path = config_folder + "/extrinsicCalib.csv";
+
+    std::vector<cv::Point2f> image_points;
+
+    if (!std::experimental::filesystem::exists(file_path)){
+          
+      std::experimental::filesystem::create_directories(config_folder);
+      
+      image_points = pickNPoints(4, img_in);
+      // SAVE POINT TO FILE
+      // std::cout << "IMAGE POINTS: " << std::endl;
+      // for (const auto pt: image_points) {
+      //   std::cout << pt << std::endl;
+      // }
+      std::ofstream output(file_path);
+      if (!output.is_open()){
+        throw std::runtime_error("Cannot write file: " + file_path);
+      }
+      for (const auto pt: image_points) {
+        output << pt.x << " " << pt.y << std::endl;
+      }
+      output.close();
+    }else{
+      // LOAD POINT FROM FILE
+      std::ifstream input(file_path);
+      if (!input.is_open()){
+        throw std::runtime_error("Cannot read file: " + file_path);
+      }
+      while (!input.eof()){
+        double x, y;
+        if (!(input >> x >> y)) {
+          if (input.eof()) break;
+          else {
+            throw std::runtime_error("Malformed file: " + file_path);
+          }
+        }
+        image_points.emplace_back(x, y);
+      }
+      input.close();
+    }
+    
+    cv::Mat dist_coeffs;
+    dist_coeffs   = (cv::Mat1d(1,4) << 0, 0, 0, 0, 0);
+    bool ok = cv::solvePnP(object_points, image_points, camera_matrix, dist_coeffs, rvec, tvec);
+
+    // cv::Mat Rt;
+    // cv::Rodrigues(rvec_, Rt);
+    // auto R = Rt.t();
+    // auto pos = -R * tvec_;
+
+    if (!ok) {
+      std::cerr << "FAILED SOLVE_PNP" << std::endl;
+    }
+
+    return ok;
+    //throw std::logic_error( "STUDENT FUNCTION - EXTRINSIC CALIB - NOT IMPLEMENTED" );   
   }
 
   void imageUndistort(const cv::Mat& img_in, cv::Mat& img_out, 
           const cv::Mat& cam_matrix, const cv::Mat& dist_coeffs, const std::string& config_folder){
+    static bool maps_initialized = false;
+    static cv::Mat full_map1, full_map2;
+    std::cout << "in student interface imageUndistort" << std::endl;
+    if(!maps_initialized){
+      // Note: m1type=CV_16SC2 to use fast fixed-point maps (see cv::remap)
+      cv::Mat R;
+      cv::initUndistortRectifyMap(cam_matrix, dist_coeffs, R, cam_matrix, 
+                                img_in.size(), CV_16SC2, full_map1, full_map2);
 
-    throw std::logic_error( "STUDENT FUNCTION - IMAGE UNDISTORT - NOT IMPLEMENTED" );  
+      maps_initialized = true;
+    }
+
+    // Initialize output image    
+    cv::remap(img_in, img_out, full_map1, full_map2, cv::INTER_LINEAR);    
+
+    //throw std::logic_error( "STUDENT FUNCTION - IMAGE UNDISTORT - NOT IMPLEMENTED" );  
 
   }
 
@@ -28,13 +189,20 @@ namespace student {
                         const cv::Mat& tvec, const std::vector<cv::Point3f>& object_points_plane, 
                         const std::vector<cv::Point2f>& dest_image_points_plane, 
                         cv::Mat& plane_transf, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - FIND PLANE TRANSFORM - NOT IMPLEMENTED" );  
+    cv::Mat image_points;
+
+    // project points
+    cv::projectPoints(object_points_plane, rvec, tvec, cam_matrix, cv::Mat(), image_points);
+
+    plane_transf = cv::getPerspectiveTransform(image_points, dest_image_points_plane);
+    //throw std::logic_error( "STUDENT FUNCTION - FIND PLANE TRANSFORM - NOT IMPLEMENTED" );  
   }
 
 
 void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, 
             const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION - UNWRAP - NOT IMPLEMENTED" );   
+    //throw std::logic_error( "STUDENT FUNCTION - UNWRAP - NOT IMPLEMENTED" ); 
+    cv::warpPerspective(img_in, img_out, transf, img_in.size());  
   }
 
   bool processMap(const cv::Mat& img_in, const double scale, std::vector<Polygon>& obstacle_list, std::vector<std::pair<int,Polygon>>& victim_list, Polygon& gate, const std::string& config_folder){
@@ -51,4 +219,3 @@ void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf,
 
 
 }
-
